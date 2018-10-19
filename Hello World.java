125e56a788b05553c55b44065e5db324dd73dd4b
John-Poulos <37157526+John-Poulos@users.noreply.github.com>
2018-10-18 21:37:53 -0400

    Create Hello World.java

---
+++ Hello World.java
@@ -0,0 +1,15 @@
+package hello.world;
+
+/**
+ * @author John
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+    
+}
