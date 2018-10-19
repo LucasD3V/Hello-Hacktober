f1f55c1195ab316f693077a81fd4f3daa3153c4a
Panagiotis Vourtsis <vourtsis_pan@hotmail.com>
2018-10-19 15:25:30 +0300

    new awesome JS helloWorld

---
+++ HelloCoffeeWorld.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const drunkCoffee = false;
+
+function helloWorld (drunkCoffee) {
+  if (drunkCoffee){
+    return ('Now i am ok so - Hello world!');
+  }
+  return ('still need coffee');
+}
