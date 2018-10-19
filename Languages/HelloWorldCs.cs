a5062f89cf57eedf38cb806a3068b602e544be2d
Patawee <maronmate@users.noreply.github.com>
2018-10-19 09:38:15 +0700

    Add file HelloWorldCs.cs

---
+++ Languages/HelloWorldCs.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HelloWorldCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello World";
+            Console.WriteLine(hello);
+            Console.Read();
+        }
+    }
+}
